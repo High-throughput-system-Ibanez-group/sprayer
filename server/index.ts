1622ccc81e218124ccf0a260c6021c08038bfc82
Carlos Bertomeu <cbertomeu18@gmail.com>
2023-03-17 14:34:19 +0100

    Open and close form nextjs server

--- server/index.ts
+++ server/index.ts
@@ -1,38 +1,38 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import http from 'http'
+import express, { Express, Request, Response } from "express"
+import dotenv from "dotenv"
+import http from "http"
 import { Server } from "socket.io"
-import { SerialPort } from 'serialport'
+import { SerialPort } from "serialport"
 
-dotenv.config();
-const app: Express = express();
-const server = http.createServer(app);
-const io = new Server(server);
-const port = process.env.PORT;
+dotenv.config()
+const app: Express = express()
+const server = http.createServer(app)
+const io = new Server(server)
+const port = process.env.PORT
 
-const serialPort = new SerialPort({
-	path: '/dev/tty.usbmodem1301',
-	baudRate: 9600,
+// const serialPort = new SerialPort({
+// 	path: '/dev/tty.usbmodem1301',
+// 	baudRate: 9600,
+// })
+// let val = false
+// setInterval(() => {
+//   console.log("in")
+//   serialPort.write(val ? "open" : "close", err => {
+//     if (err) {
+//       return console.log('Error on write: ', err?.message)
+//     }
+//   })
+//   val = !val
+// }, 10000)
+
+io.on("connection", (socket) => {
+  console.log("a user connected")
 })
-let val = false
-setInterval(() => {
-  console.log("in")
-  serialPort.write(val ? "open" : "close", err => {
-    if (err) {
-      return console.log('Error on write: ', err?.message)
-    }
-  })
-  val = !val
-}, 10000)
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server")
+})
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+})
---
+++ web/src/pages/api/socket.ts
@@ -0,0 +1,46 @@
+import type { Server as HTTPServer } from "http";
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { Socket as NetSocket } from "net";
+import { Server as IOServer } from "socket.io";
+// import { SerialPort } from "serialport";
+
+interface SocketServer extends HTTPServer {
+  io?: IOServer | undefined;
+}
+
+interface SocketWithIO extends NetSocket {
+  server: SocketServer;
+}
+
+interface NextApiResponseWithSocket extends NextApiResponse {
+  socket: SocketWithIO;
+}
+
+const SocketHandler = (req: NextApiRequest, res: NextApiResponseWithSocket) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  if (res.socket.server.io) {
+    console.log("Socket is already running");
+  } else {
+    console.log("Socket is initializing");
+    const io = new IOServer(res.socket.server);
+    res.socket.server.io = io;
+    // void SerialPort.list().then(function (ports) {
+    //   ports.forEach(function (port) {
+    //     console.log("Portttt: ", port);
+    //   });
+    // });
+
+    io.on("connection", (socket) => {
+      socket.on("blink", (msg) => {
+        if (msg === "open") {
+          console.log("OPEN");
+        } else if (msg === "close") {
+          console.log("CLOSE");
+        }
+      });
+    });
+  }
+  res.end();
+};
+
+export default SocketHandler;
