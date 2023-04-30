e9873d5d1a7037a1eecead1653be41399925ae71
Carlos Bertomeu <cbertomeu18@gmail.com>
2023-04-30 14:32:11 +0200

    Add types

--- web/src/utils/types.ts
+++ web/src/utils/types.ts
@@ -1,1 +1,13 @@
 export type Axis = "x" | "y" | "z";
+
+// coordinates type
+export type Coordinates = {
+  x: number;
+  y: number;
+  z: number;
+};
+
+export type Area = {
+  startingPoint: Coordinates;
+  endingPoint: Coordinates;
+};
