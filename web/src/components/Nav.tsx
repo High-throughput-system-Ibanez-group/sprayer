c42667ef927e510a3f6e9c50adda8201d2e22e3e
Carlos Bertomeu <cbertomeu18@gmail.com>
2023-04-30 14:30:45 +0200

    Add link to nav

--- web/src/components/Nav.tsx
+++ web/src/components/Nav.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 
@@ -8,20 +9,24 @@ const Nav = () => {
     <nav className="flex items-center bg-gray-800 p-4 text-white">
       <div className="text-xl font-bold">Sprayer</div>
       <div className="w-6" />
-      <div
-        className={`mx-4 cursor-pointer rounded-lg px-4 py-2 hover:bg-slate-500 ${
-          router.pathname === "/" ? "underline" : ""
-        }`}
-      >
-        Control Panel
-      </div>
-      <div
-        className={`mx-4 cursor-pointer rounded-lg px-4 py-2 hover:bg-slate-500 ${
-          router.pathname === "/area-configuration" ? "underline" : ""
-        }`}
-      >
-        Area Configuration
-      </div>
+      <Link href="/">
+        <div
+          className={`mx-4 cursor-pointer rounded-lg px-4 py-2 hover:bg-slate-500 ${
+            router.pathname === "/" ? "underline" : ""
+          }`}
+        >
+          Control Panel
+        </div>
+      </Link>
+      <Link href="/area-config">
+        <div
+          className={`mx-4 cursor-pointer rounded-lg px-4 py-2 hover:bg-slate-500 ${
+            router.pathname === "/area-config" ? "underline" : ""
+          }`}
+        >
+          Area Configuration
+        </div>
+      </Link>
     </nav>
   );
 };
