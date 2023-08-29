fec83715f39534afb54d5b0d067ab29ce356f42f
Carlos Bertomeu <cbertomeu18@gmail.com>
2023-08-29 19:26:34 +0200

    WIP: Ultrasonic generator

--- app/src/pages/api/socket.ts
+++ app/src/pages/api/socket.ts
@@ -4,6 +4,11 @@ import type { Socket as NetSocket } from "net";
 import { Server as IOServer } from "socket.io";
 import { SerialPort } from "serialport";
 import { ReadlineParser } from "@serialport/parser-readline";
+import {
+  type DataTypeUltra,
+  formatSendDataUltrasonic,
+  parseReceivedDataUltrasonic,
+} from "~/utils/ultrasonicSensor/functions";
 
 interface SocketServer extends HTTPServer {
   io?: IOServer | undefined;
@@ -21,6 +26,28 @@ const arduinoSerialPort = new SerialPort({
   path: process.env.ARDUINO_PORT_PATH || "COM10",
   baudRate: 9600,
 });
+
+// Configure SerialPort for RS485 ultrasonic sensor communication
+const serialPortUltra = new SerialPort({
+  path: process.env.ULTRASONIC_PORT_PATH || "COM10",
+  baudRate: 9600,
+  dataBits: 8,
+  parity: "none",
+  stopBits: 1,
+});
+
+const parserUltra = serialPortUltra.pipe(new ReadlineParser());
+
+// Function to send data through SerialPort
+const sendDataUltra = (data: Buffer) => {
+  serialPortUltra.write(data, (err) => {
+    if (err) {
+      console.error("Error sending data:", err);
+    } else {
+      console.log("Data sent:", data);
+    }
+  });
+};
 
 const arduinoReadlineParser = new ReadlineParser({ delimiter: "\r\n" });
 
@@ -51,11 +78,25 @@ const SocketHandler = (_: NextApiRequest, res: NextApiResponseWithSocket) => {
       console.log("receivedCommand: ", receivedCommand);
     });
 
+    // Event handler for receiving data from the SerialPort
+    parserUltra.on("data", (data: Buffer) => {
+      // Process the received data following the given instructions
+      const parsedData = parseReceivedDataUltrasonic(data);
+      console.log("Received Ultrasonic Data:", parsedData);
+    });
+
     io.on("connection", (socket) => {
       console.log("Socket connected");
       socket.on("sendCommand", (command: string) => {
         arduinoSerialPort.write(`${command}\n`, handlePortError);
         console.log("sendCommand: ", command);
+      });
+
+      // Event handler for sending data from the client to the RS485 device
+      socket.on("sendDataToRS485", (data: DataTypeUltra) => {
+        // Format the data based on the given instructions
+        const formattedData = formatSendDataUltrasonic(data);
+        sendDataUltra(formattedData);
       });
     });
 
